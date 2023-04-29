84c8939760f3acdc458748bfc5c64ffe0d026599
anilib <artemy1986d@gmail.com>
2023-04-29 11:48:10 +0700

    now

--- code/chat_message/main.py
+++ code/chat_message/main.py
@@ -41,61 +41,34 @@ class instance(QMainWindow):
             self.move(self.pos() + delta)
 
         def update(self) -> None:
-            updating_settings(self=self)
-
-        def update_colors(self):
-            global custom_colors, dark, system_update
-            background = self.background.text()
-            border = self.border.text()
-            foreground = self.foreground.text()
-            primary = self.primary.text()
-            input_background = self.input_background.text()
-            input_button_hover_background = self.inputButton_hoverBackground.text()
-
+            global decode
+            self.setGeometry(1455, 650, 200, 70)
+            self.setWindowTitle('UNKNOWN INCOMING')
+            self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
             self.setStyleSheet(qdarktheme.load_stylesheet(custom_colors={"primary": "#D0BCFF"}))
-            custom_colors = {"background": f"{background}",
-                             "border": f"{border}",
-                             "foreground": f"{foreground}",
-                             "primary": f"{primary}",
-                             "input.background": f"{input_background}",
-                             "inputButton.hoverBackground": f"{input_button_hover_background}"}
-            self.setStyleSheet(qdarktheme.load_stylesheet(custom_colors=custom_colors))
-            dark = 3
-            system_update = True
+
+            self.btn_registration = QPushButton('updating the decoder', self)
+            self.btn_registration.resize(self.btn_registration.sizeHint())
+            self.btn_registration.move(0, 10)
+            self.btn_registration.resize(self.btn_registration.sizeHint())
+            self.btn_registration.clicked.connect(self.NowDecode)
+            self.encode = QLineEdit(self)
+            self.encode.setText(str(decode))
+            self.encode.resize(70, 25)
+            self.encode.move(130, 10)
+
+            self.exit = QPushButton('exit', self)
+            self.exit.clicked.connect(self.yes_no)
+            self.exit.resize(self.exit.sizeHint())
+            self.exit.resize(75, 24)
+            self.exit.move(0, 35)
+            self.exit.setVisible(True)
 
         def yes_no(self):
-            global dark, custom_colors
-            if self.checkbox.isChecked():
-                dark = 1
-                self.setStyleSheet(qdarktheme.load_stylesheet("light"))
-            if not self.checkbox.isChecked():
-                dark = 2
-                self.setStyleSheet(qdarktheme.load_stylesheet(custom_colors={"primary": "#D0BCFF"}))
+            self.close()
+            self.open1 = True
 
-        def now_ip(self, s):
-            global ip, Address, update_mas, update_ip
-
-            self.reply = QMessageBox(self)
-            self.reply.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
-
-            self.reply.move(self.geometry().x(), self.geometry().y()-100)
-            self.reply.setText("Вы уверены что хотите сменить ipaddress")
-            self.reply.setStandardButtons(QMessageBox.StandardButton.Yes |
-                                     QMessageBox.StandardButton.No)
-            self.reply.setIcon(QMessageBox.Icon.Question)
-            x = self.reply.exec()
-
-            if x == QMessageBox.StandardButton.Yes:
-                try:
-                    ip = str(self.interlocutor_ip.text())
-                    Address = int(self.interlocutor_adress.text())
-                    update_ip = True
-                    update_mas = True
-                except ValueError:
-                    self.interlocutor_ip.setText("interlocutor ip")
-                    self.interlocutor_adress.setText("Address")
-
-        def exit_menu(self):
+        def NowDecode(self):
             global decode
             try:
                 decode = int(self.encode.text())
@@ -128,88 +101,94 @@ class instance(QMainWindow):
             self.move(self.pos() + delta)
 
         def update(self):
-            self.setGeometry(1455, 650, 200, 250)
+            self.setGeometry(1455, 650, 200, 210)
             self.setWindowTitle('UNKNOWN INCOMING')
             self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
             self.setStyleSheet(qdarktheme.load_stylesheet(custom_colors={"primary": "#D0BCFF"}))
 
             self.checkbox = QCheckBox("white theme", self)
-            self.checkbox.move(0, 50)
+            self.checkbox.move(0, 0)
             self.checkbox.resize(self.checkbox.sizeHint())
             self.checkbox.clicked.connect(self.yes_no)
 
             self.background = QLineEdit(self)
             self.background.setText("#bcacd4")
             self.background.resize(110, 20)
-            self.background.move(0, 75)
+            self.background.move(0, 25)
 
             self.border = QLineEdit(self)
             self.border.setText("#d596fa")
             self.border.resize(110, 20)
-            self.border.move(0, 100)
+            self.border.move(0, 50)
 
             self.foreground = QLineEdit(self)
             self.foreground.setText("#18047e")
             self.foreground.resize(110, 20)
-            self.foreground.move(0, 125)
+            self.foreground.move(0, 75)
 
             self.primary = QLineEdit(self)
             self.primary.setText("#8624e4")
             self.primary.resize(110, 20)
-            self.primary.move(0, 150)
+            self.primary.move(0, 100)
 
             self.input_background = QLineEdit(self)
             self.input_background.setText("#d596fa")
             self.input_background.resize(110, 20)
-            self.input_background.move(0, 175)
+            self.input_background.move(0, 125)
 
             self.inputButton_hoverBackground = QLineEdit(self)
             self.inputButton_hoverBackground.setText("#18047e")
             self.inputButton_hoverBackground.resize(110, 20)
-            self.inputButton_hoverBackground.move(0, 200)
+            self.inputButton_hoverBackground.move(0, 150)
 
             self.text_background = QLabel(self)
             self.text_background.setText('background')
             self.text_background.resize(self.text_background.sizeHint())
-            self.text_background.move(115, 75)
+            self.text_background.move(115, 25)
 
             self.text_border = QLabel(self)
             self.text_border.setText('border')
             self.text_border.resize(self.text_border.sizeHint())
-            self.text_border.move(115, 100)
+            self.text_border.move(115, 50)
 
             self.text_foreground = QLabel(self)
             self.text_foreground.setText('foreground')
             self.text_foreground.resize(self.text_foreground.sizeHint())
-            self.text_foreground.move(115, 125)
+            self.text_foreground.move(115, 75)
 
             self.text_primary = QLabel(self)
             self.text_primary.setText('primary')
             self.text_primary.resize(self.text_primary.sizeHint())
-            self.text_primary.move(115, 150)
+            self.text_primary.move(115, 100)
 
             self.text_input_background = QLabel(self)
             self.text_input_background.setText('input')
             self.text_input_background.resize(self.text_input_background.sizeHint())
-            self.text_input_background.move(115, 172)
+            self.text_input_background.move(115, 122)
             self.text_input_background = QLabel(self)
             self.text_input_background.setText('background')
             self.text_input_background.resize(self.text_input_background.sizeHint())
-            self.text_input_background.move(115, 180)
+            self.text_input_background.move(115, 130)
 
             self.text_inputButton_hoverBackground = QLabel(self)
             self.text_inputButton_hoverBackground.setText('inputButton')
             self.text_inputButton_hoverBackground.resize(self.text_inputButton_hoverBackground.sizeHint())
-            self.text_inputButton_hoverBackground.move(115, 197)
+            self.text_inputButton_hoverBackground.move(115, 147)
             self.text_inputButton_hoverBackground = QLabel(self)
             self.text_inputButton_hoverBackground.setText('Background')
             self.text_inputButton_hoverBackground.resize(self.text_inputButton_hoverBackground.sizeHint())
-            self.text_inputButton_hoverBackground.move(115, 205)
+            self.text_inputButton_hoverBackground.move(115, 155)
 
             self.text_foreground = QPushButton('UPDATE_COLORS', self)
             self.text_foreground.clicked.connect(self.update_colors)
             self.text_foreground.resize(self.text_foreground.sizeHint())
-            self.text_foreground.move(0, 225)
+            self.text_foreground.move(0, 175)
+
+            self.exit = QPushButton('✖', self)
+            self.exit.clicked.connect(self.yes_no)
+            self.exit.resize(self.exit.sizeHint())
+            self.exit.move(170, 0)
+            self.exit.setVisible(True)
 
 
         def update_colors(self):
@@ -231,15 +210,9 @@ class instance(QMainWindow):
             self.setStyleSheet(qdarktheme.load_stylesheet(custom_colors=custom_colors))
             dark = 3
             system_update = True
-
         def yes_no(self):
-            global dark, custom_colors
-            if self.checkbox.isChecked():
-                dark = 1
-                self.setStyleSheet(qdarktheme.load_stylesheet("light"))
-            if not self.checkbox.isChecked():
-                dark = 2
-                self.setStyleSheet(qdarktheme.load_stylesheet(custom_colors={"primary": "#D0BCFF"}))
+            self.close()
+            self.open1 = True
 
     class SettingWindowMenuipaddress(QWidget):
         global ip, Address, update_mas, update_ip
